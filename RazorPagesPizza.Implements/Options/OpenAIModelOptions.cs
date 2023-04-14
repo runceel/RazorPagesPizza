bcc64c9ce009f4394ece028bc49fa8d294a3c670
Kazuki Ota <k_ota28@hotmail.com>
2023-04-15 08:07:07 +0900

    wip

---
+++ RazorPagesPizza.Implements/Options/OpenAIModelOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace RazorPagesPizza.Repositories.Options;
+
+public class OpenAIModelOptions
+{
+    public required string DeployModelName { get; set; }
+}
--- RazorPagesPizza.Implements/Options/OpenAIOptions.cs
+++ RazorPagesPizza.Implements/Options/OpenAIOptions.cs
@@ -4,5 +4,4 @@ public class OpenAIOptions
 {
     public required string Endpoint { get; set; }
     public required string Key { get; set; }
-    public required string DeployModelName { get; set; }
 }
