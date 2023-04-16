c161c268edcf9092df3c078ab5b9466db3d2492c
Kazuki Ota <k_ota28@hotmail.com>
2023-04-16 11:26:23 +0900

    update

--- RazorPagesPizza.Functions/GeneratePizzaDescriptionFunction.cs
+++ RazorPagesPizza.Functions/GeneratePizzaDescriptionFunction.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Azure.Functions.Worker;
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using RazorPagesPizza.Core.Services;
 using RazorPagesPizza.Implements.Repositories;
 
-namespace RazorPagesPizza.Functions
+namespace RazorPagesPizza.Functions;
+
+public class GeneratePizzaDescriptionFunction
 {
-    public class GeneratePizzaDescriptionFunction
+    private readonly ILogger _logger;
+    private readonly IPizzaDescriptionGenerateService _pizzaDescriptionGenerateService;
+
+    public GeneratePizzaDescriptionFunction(IPizzaDescriptionGenerateService pizzaDescriptionGenerateService, ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
-        private readonly IPizzaDescriptionGenerateService _pizzaDescriptionGenerateService;
+        _logger = loggerFactory.CreateLogger<GeneratePizzaDescriptionFunction>();
+        _pizzaDescriptionGenerateService = pizzaDescriptionGenerateService;
+    }
 
-        public GeneratePizzaDescriptionFunction(IPizzaDescriptionGenerateService pizzaDescriptionGenerateService, ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<GeneratePizzaDescriptionFunction>();
-            _pizzaDescriptionGenerateService = pizzaDescriptionGenerateService;
-        }
-
-        [Function("GeneratePizzaDescriptionFunction")]
-        public async Task Run([QueueTrigger(PizzaQueue.Name, Connection = "PizzaQueue")] GeneratePizzaDescriptionRequest request)
-        {
-            _logger.LogInformation($"C# Queue trigger function processed: {request.PizzaId}");
-            await _pizzaDescriptionGenerateService.GenerateDescriptionAsync(new(request.PizzaId, request.PizzaName));
-        }
+    [FunctionName("GeneratePizzaDescriptionFunction")]
+    public async Task Run([QueueTrigger(PizzaQueue.Name, Connection = "PizzaQueue")] GeneratePizzaDescriptionRequest request)
+    {
+        _logger.LogInformation($"C# Queue trigger function processed: {request.PizzaId}");
+        await _pizzaDescriptionGenerateService.GenerateDescriptionAsync(new(request.PizzaId, request.PizzaName));
     }
 }
